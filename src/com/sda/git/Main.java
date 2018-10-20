1ce110bffdb489486e441fba38938add6d526844
Kolachow <kolas3000@gmail.com>
2018-10-20 15:03:42 +0200

    This is second_copy

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Yo");
-        System.out.println("Aktualny branch to: feature");
+        System.out.println("Aktualny branch to: second_copy");
     }
 }
