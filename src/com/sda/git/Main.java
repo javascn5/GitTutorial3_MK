3592cff6a9dc2599659dd7c2b51df0a6f92e1f7b
Kolachow <kolas3000@gmail.com>
2018-10-20 15:26:44 +0200

    Added cecond loop

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -9,5 +9,9 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("test nr: " + (i + 1));
         }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test nr: " + (10 - i));
+        }
     }
 }
