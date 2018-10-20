e2f873fd3d8647fd947875abb993ff9271aa23a3
Kolachow <kolas3000@gmail.com>
2018-10-20 15:22:04 +0200

    Add loop

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Yo");
         System.out.println("Aktualny branch to: second_copy");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test nr: " + (i + 1));
+        }
     }
 }
